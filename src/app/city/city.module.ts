5f20ca0aabdb19dd072caeff97b4e28a73ef443a
Umme-hanii <ummehanimohammed9@gmail.com>
2023-12-05 13:56:45 +0530

    Directive to display dynamic images

--- src/app/city/city.module.ts
+++ src/app/city/city.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { RouterModule } from '@angular/router'
+import { ReactiveFormsModule } from '@angular/forms'
+
 import { CityDetailsComponent } from './city-details/city-details.component'
 import { CityListComponent } from './city-list/city-list.component'
 import { MaterialDesignModule } from '../modules/material-design.module'
-import { ReactiveFormsModule } from '@angular/forms'
-import { RouterModule } from '@angular/router'
-import { CommonModule } from '@angular/common'
 import { CityComponent } from './city.component'
 import { CityRoutingModule } from './city-routing.module'
 import { CityBannerComponent } from './city-banner/city-banner.component'
+import { backgroundImageDirective } from '../shared/directives/backgroundImage.directive'
 
 @NgModule({
   declarations: [
+    backgroundImageDirective,
     CityDetailsComponent,
     CityListComponent,
     CityComponent,
---
+++ src/app/shared/directives/backgroundImage.directive.ts
@@ -0,0 +1,23 @@
+import { Directive, ElementRef, Input, OnInit, Renderer2 } from '@angular/core'
+@Directive({
+  selector: '[backgroundImage]',
+})
+export class backgroundImageDirective implements OnInit {
+  imageUrl!: string
+  @Input() imageName: string = 'banner-img'
+
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
+
+  ngOnInit() {
+    if (this.imageName.includes(' ')) {
+      this.imageName = this.imageName.slice(0, this.imageName.indexOf(' '))
+    }
+    this.imageUrl =
+      '../../../../assets/images/' + this.imageName.toLowerCase() + '.jpg'
+    this.renderer.setStyle(
+      this.elementRef.nativeElement,
+      'backgroundImage',
+      `url(${this.imageUrl})`
+    )
+  }
+}
