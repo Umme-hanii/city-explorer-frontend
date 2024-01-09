d61030a3e2fa5d0347bdbab08ccd613b13ea51f4
Umme-hanii <Umme.Hani@quest.com>
2024-01-09 20:58:02 +0530

    production env variables

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  API_URL: 'https://city-explorer-backend-aiew.onrender.com/api/cities',
+  MAP_API_URL: 'https://www.google.com/maps/embed/v1/directions',
+  MAP_API_KEY: 'AIzaSyCddTuJmKuE4IyWkAH5Q8V2ybtwGiwc6xw',
+}
