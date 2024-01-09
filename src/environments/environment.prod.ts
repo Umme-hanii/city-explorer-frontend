3389b007ab65b81fb3c51c80d16b43b27c6c7170
Umme-hanii <ummehanimohammed9@gmail.com>
2024-01-09 21:08:34 +0530

    set production to false

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   API_URL: 'https://city-explorer-backend-aiew.onrender.com/api/cities',
   MAP_API_URL: 'https://www.google.com/maps/embed/v1/directions',
   MAP_API_KEY: 'AIzaSyCddTuJmKuE4IyWkAH5Q8V2ybtwGiwc6xw',
